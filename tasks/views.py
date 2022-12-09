50fdb1bdb22399fe04c30948fcfd5f8a11a9fa70
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-12-08 22:03:11 -0300

    Forms and models

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 def taskList(request):
-    tasks = Task.objects.all()        
+    tasks = Task.objects.all().order_by('-created_at')        
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
@@ -15,3 +16,18 @@ def yourName(request, name):
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
+
+def newTask(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+            
+    else:        
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
