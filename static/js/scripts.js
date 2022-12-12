6d906d95cd72bf0f65866db966979b89088676e1
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-12-12 19:16:57 -0300

    edit, delete, flash_messages and JS

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,15 @@
-console.log("Funcionou")
+$(document).ready(function(){
+   
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e){
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+});
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator  #lib para paginação
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
-
+from django.contrib import messages
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')        
+    tasks_list = Task.objects.all().order_by('-created_at')  
+          
+    paginator = Paginator(tasks_list, 3)
+    
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
+    
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
@@ -26,8 +33,35 @@ def newTask(request):
             task = form.save(commit=False)
             task.done = 'doing'
             task.save()
+            
+            messages.info(request, 'Tarefa criada com sucesso.')
             return redirect('/')
             
     else:        
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
+    
+    
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)   #para listar a task específica
+    
+    if(request.method == "POST"):
+        form = TaskForm(request.POST, instance=task)
+        if(form.is_valid()):
+            task.save()
+            
+            messages.info(request, 'Tarefa editada com sucesso.')
+            return redirect('/')
+        else:            
+            return render(request, 'tasks/edittask.html', {'form': form, 'taks': task})
+    else:        
+        return render(request, 'tasks/edittask.html', {'form': form, 'taks': task})
+    
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    
+    messages.info(request, 'Tarefa deletada com sucesso.')
+    return redirect('/')
